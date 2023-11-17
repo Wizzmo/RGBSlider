811ca45652ab431f1ff261955068b3b81ad6980e
Maxim Makarov <makarovmaxqa@gmail.com>
2023-11-17 16:01:49 +0100

    Initial Commit

---
+++ RGBSlider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGBSlider
+//
+//  Created by Maxim Makarov on 17.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
