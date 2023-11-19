23d986f7182dee84db2382723a58ed679781436f
Maxim Makarov <makarovmaxqa@gmail.com>
2023-11-19 18:41:37 +0100

    Setup logic for sliders and view

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -21,15 +21,49 @@ final class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLabels()
+        setupColourPanelView()
     }
     
     @IBAction func redSliderValueChanged() {
+        redColourIntensityLabel.text = String(format: "%.2f", redSlider.value)
+        setupColourPanelView()
     }
     
     @IBAction func greenSliderValueChanged() {
+        greenColourIntensityLabel.text = String(format: "%.2f", greenSlider.value)
+        setupColourPanelView()
     }
     
     @IBAction func blueSliderValueChanged() {
+        blueColourIntensityLabel.text = String(format: "%.2f", blueSlider.value)
+        setupColourPanelView()
     }
+    
+    private func setupLabels() {
+        redColourIntensityLabel.text = redSlider.value.formatted()
+        greenColourIntensityLabel.text = greenSlider.value.formatted()
+        blueColourIntensityLabel.text = greenSlider.value.formatted()
+    }
+    
+    private func setupColourPanelView() {
+        colourPanelView.layer.cornerRadius = 30
+        
+        let redColourValue = CGFloat(redSlider.value)
+        let greenColourValue = CGFloat(greenSlider.value)
+        let blueColourValue = CGFloat(blueSlider.value)
+        
+        colourPanelView.backgroundColor = UIColor(
+            red: redColourValue,
+            green: greenColourValue,
+            blue: blueColourValue,
+            alpha: 1.0
+        )
+    }
+    
+    @IBAction func changeSliderValue() {
+    
+    }
+    
 }
 
