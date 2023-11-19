c982aa86e498cff514254a831d52beb072c83089
Maxim Makarov <makarovmaxqa@gmail.com>
2023-11-19 19:46:38 +0100

    Fix in logic

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -21,49 +21,43 @@ final class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupLabels()
         setupColourPanelView()
+        changeViewBackgroundColour()
+        setupLabels()
     }
     
     @IBAction func redSliderValueChanged() {
+        changeViewBackgroundColour()
         redColourIntensityLabel.text = String(format: "%.2f", redSlider.value)
-        setupColourPanelView()
     }
     
     @IBAction func greenSliderValueChanged() {
+        changeViewBackgroundColour()
         greenColourIntensityLabel.text = String(format: "%.2f", greenSlider.value)
-        setupColourPanelView()
     }
     
     @IBAction func blueSliderValueChanged() {
+        changeViewBackgroundColour()
         blueColourIntensityLabel.text = String(format: "%.2f", blueSlider.value)
-        setupColourPanelView()
     }
     
     private func setupLabels() {
         redColourIntensityLabel.text = redSlider.value.formatted()
         greenColourIntensityLabel.text = greenSlider.value.formatted()
-        blueColourIntensityLabel.text = greenSlider.value.formatted()
+        blueColourIntensityLabel.text = blueSlider.value.formatted()
     }
     
     private func setupColourPanelView() {
-        colourPanelView.layer.cornerRadius = 30
-        
-        let redColourValue = CGFloat(redSlider.value)
-        let greenColourValue = CGFloat(greenSlider.value)
-        let blueColourValue = CGFloat(blueSlider.value)
-        
+        colourPanelView.layer.cornerRadius = 20
+    }
+    
+    private func changeViewBackgroundColour() {
         colourPanelView.backgroundColor = UIColor(
-            red: redColourValue,
-            green: greenColourValue,
-            blue: blueColourValue,
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
             alpha: 1.0
         )
     }
-    
-    @IBAction func changeSliderValue() {
-    
-    }
-    
 }
 
